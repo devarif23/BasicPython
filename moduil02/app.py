4ad1cd4dd03fe3339ba2addf391725307d696ab6
arif jahan <mdarifjahan138@gmail.com>
2025-09-16 06:49:26 +0600

    dhfd

---
+++ moduil02/app.py
@@ -0,0 +1,26 @@
+# 2.1 Introduction to String
+
+# String length বের করো "Python" → Output: 6
+x = 'ariaja'
+print(len(x))
+
+# Uppercase এ রূপান্তর "python programming" → Output: "PYTHON PROGRAMMING"
+a = 'python programming'
+
+upper = a.upper()
+
+print(upper)
+
+
+# String concatenate "Hello" + "World" → Output: "Hello World"
+a = "Hello"
+b = "World"
+
+print(a  + " " + b)
+# or
+print(f"{a} {b}")
+
+# Access character "Python" ৩য় index → Output: "h"
+
+x = "Python" 
+print(x[3])
